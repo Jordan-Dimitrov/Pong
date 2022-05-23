b5f60c9cdddbd0d253e0ebae36d528dd8a1a1eca
MrLoveMilk <dani27game@gmail.com>
2022-05-23 20:11:27 +0300

    Paddles

--- Mono/Game1.cs
+++ Mono/Game1.cs
@@ -4,10 +4,14 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Mono
 {
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _tex;
+        private Rectangle _transform;
+        private Rectangle _transform1;
 
         public Game1()
         {
@@ -25,27 +29,43 @@ namespace Mono
 
         protected override void LoadContent()
         {
+            // 1111 1111
+            // 255 = ff
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            _tex = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+            uint[] data = new uint[] { 0xffffffff };
+            _tex.SetData(data);
+            _transform = new Rectangle(0, 100, 50, 200);
+            _transform1 = new Rectangle(750, 100, 50, 200);
+            // TODO: use this.Content to load your game content 
+            base.LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
-
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                _transform.Y -= 10;
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+                _transform.Y += 10;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                _transform1.Y -= 10;
+            else if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                _transform1.Y += 10;
+            // TODO: Add your update logic 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_tex, _transform, Color.White);
+            _spriteBatch.Draw(_tex, _transform1, Color.White);
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
-
             base.Draw(gameTime);
         }
     }
